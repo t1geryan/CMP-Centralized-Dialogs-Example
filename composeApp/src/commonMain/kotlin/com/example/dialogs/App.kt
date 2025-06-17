8e809d45eecbd2ead988677d7c5e2523826f8b9d
tigran.movsisyan <tigran.movsisyan@dsr-corporation.com>
2025-06-17 16:40:40 +0300

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/com/example/dialogs/App.kt
@@ -0,0 +1,13 @@
+package com.example.dialogs
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
