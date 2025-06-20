f726c1802f40ee0d875f925e369a70e7c9eb364a
tigran.movsisyan <tigran.movsisyan@dsr-corporation.com>
2025-06-20 16:48:24 +0300

    Added important comment

--- composeApp/src/commonMain/kotlin/com/example/dialogs/presentation/dialogs/DialogHolder.kt
+++ composeApp/src/commonMain/kotlin/com/example/dialogs/presentation/dialogs/DialogHolder.kt
@@ -5,5 +5,8 @@ import com.arkivanov.decompose.value.Value
 interface DialogHolder {
     val isDialogOpen: Value<Boolean>
 
+    /**
+     * **It is strongly recommended to call this method on the Main thread.**
+     */
     fun showDialog(model: DialogModel)
 }
